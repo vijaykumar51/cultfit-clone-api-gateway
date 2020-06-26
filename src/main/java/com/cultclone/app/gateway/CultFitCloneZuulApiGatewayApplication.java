7c238be933dea26c909b7992dccdb1f33d228514
Vijay Kumar <vijay.kumar.em51@gmail.com>
2020-06-26 23:24:16 +0530

    Adding initial zuul api gateway project

---
+++ src/main/java/com/cultclone/app/gateway/CultFitCloneZuulApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cultclone.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultFitCloneZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultFitCloneZuulApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cultclone/app/gateway/CultFitCloneZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cultclone.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultFitCloneZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
