9b9c766a296cdb27dafd70ed92be368107684b24
Vijay Kumar <vijay.kumar.em51@gmail.com>
2020-06-28 13:42:24 +0530

    Adding basic routing in api-gateway

--- src/main/java/com/cultclone/app/gateway/CultFitCloneZuulApiGatewayApplication.java
+++ src/main/java/com/cultclone/app/gateway/CultFitCloneZuulApiGatewayApplication.java
@@ -2,10 +2,20 @@ package com.cultclone.app.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * The Class CultFitCloneZuulApiGatewayApplication.
+ */
 @SpringBootApplication
+@EnableZuulProxy
 public class CultFitCloneZuulApiGatewayApplication {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CultFitCloneZuulApiGatewayApplication.class, args);
 	}
